8a9dbbf0e71e5178f5959ba12498c087dfc833dd
JouJou411 <ajaaziel73@gmail.com>
2021-12-13 12:47:40 -0600

    inicio del proyecto

---
+++ ProyectoAereopuerto/src/domain/Boletos.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joabp
+ */
+public class Boletos implements Serializable{
+//    private int 
+}
